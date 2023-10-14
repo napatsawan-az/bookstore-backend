e6867f657a5be107fa34a7bce9bf7fa372f55253
Napatsawan <napatsawan.az@gmail.com>
2023-10-14 17:54:01 +0700

    Add CORS middleware to handle cross-origin requests

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import bookRoute from "./routes/bookRoute.js";
+import cors from "cors";
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CROS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+// app.use(cors());
+// Option 2: Allow Custom Origins
+app.use(
+  cors({
+    origins: "http://localhost:5000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.get("/", (request, response) => {
   console.log(request);
